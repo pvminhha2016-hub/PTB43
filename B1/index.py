81c0b764d2d24aa4bd2e403fa5d5ae683fda5b43
pvminhha2016-hub <pvminhha2016@gmail.com>
2025-11-30 14:39:09 +0700

    first commit

---
+++ B1/index.py
@@ -0,0 +1,13 @@
+# Nhập thông tin cơ bản -> in ra màn hình
+# đặt biến name để lưu lại tên được nhập vào
+## ten
+name = input("ban ten gi")
+#tuoi
+age = 12
+#so thinh
+hobbies = input("play chess")
+# in ra màn hình
+print(f"""Xin chao ban {name}, 
+      tuoi cua ban la {age} 
+      va co so thich la {hobbies}.""")
+
